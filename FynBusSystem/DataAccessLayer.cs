efca8a5a2079b02faa7fe4d86d8529e8c935b150
KMilaszkiewicz <k.archeress@gmail.com>
2016-04-07 13:12:18 +0200

    Added database connection and LoadColumnsFromDatabase method

--- FynBusSystem/DataAccessLayer.cs
+++ FynBusSystem/DataAccessLayer.cs
@@ -3,22 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace FynBusSystem
 {
     class DataAccessLayer
     {
-        public void DatabaseConnection()
+        static private SqlConnection DatabaseConnection()
         {
-
-        }
+            SqlConnection connection = new SqlConnection(
+                "Server=ealdb1.eal.local;" +
+                "Database=EJL63_DB;" +
+                "User Id=ejl63_usr;" +
+                "Password=Baz1nga63;");
+            return connection;
+    }
         public void SaveSpreadSheetRow()
         {
 
         }
         public void LoadColumnsFromDatabase()
         {
+            SqlConnection connection = DatabaseConnection();
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("LoadColumns", connection);
+                command.CommandType = CommandType.StoredProcedure;
 
+                command.ExecuteNonQuery();
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                connection.Close();
+                connection.Dispose();
+            }
         }
     }
 }
